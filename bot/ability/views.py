28b772f8d25ab242806b412e9b5ae8fd14dc3032
Bryan Ducourt <yuchen200008@qq.com>
2023-03-04 01:41:11 +0800

    tt

--- bot/ability/views.py
+++ bot/ability/views.py
@@ -17,10 +17,8 @@ def test(request):
             uid = param['sender']['user_id']
             message = raw_message.split()
             if len(message)!=1:
-                code = message[0][4:6]
-                # print(param)
-                # print(code, message[0][10:-1],param['self_id'])
-                if code=='at' and message[0][10:-1]==param['self_id']:
+                code = message[0][1:]
+                if code=='jiba':
                     resp['group_id'] = gid
                     resp['message'] = f'[CQ:at,qq={uid}]爬啊你个寄吧'
                     r = requests.post("http://127.0.0.1:5700/send_group_msg",data=resp)
