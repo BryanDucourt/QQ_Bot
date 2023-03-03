a7721c2b0c7dc5b5fead899da57b6fce55a90a77
Bryan Ducourt <yuchen200008@qq.com>
2023-03-04 00:48:22 +0800

    tt

--- bot/ability/views.py
+++ bot/ability/views.py
@@ -7,14 +7,13 @@ def test(request):
     resp ={}
 
     param = json.loads(request.body)
-    print(param['post_type'])
     if param['post_type'] == 'message':
-        print(param['message_type'])
         if param['message_type']=='group':
             gid = param['group_id']
             uid = param['sender']['user_id']
             message = param['raw_message']
             resp['group_id'] = gid
             resp['message'] = message
-            requests.post("http://127.0.0.1:5700/send_group_message",data=resp)
+            r = requests.post("http://127.0.0.1:5700/send_group_message",data=resp)
+            print(r.json())
     return JsonResponse({})
